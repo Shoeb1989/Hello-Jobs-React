6923d06b4ca3000a481f229ad60ad8544ce5f141
Shoeb1989 <psikder09@gmail.com>
2024-09-07 22:46:14 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import Root from "./COMPONENTS/Root/Root";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Home from "./COMPONENTS/Home/Home";
+import AppliedJobs from "./COMPONENTS/AppliedJob/AppliedJobs";
+import Error from "./COMPONENTS/Error/Error";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement:<Error></Error>,
+    children:[
+      {
+        path:'/',
+        element: <Home></Home>
+      },
+      {
+        path:'/applied',
+        element: <AppliedJobs></AppliedJobs>
+      },
+    ]
+  },
+
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
